8045ae82541764f655b5dc43758db9e5f9b0305a
Mahir Rahman <mahirrahman2000@gmail.com>
2023-09-14 22:44:48 +0100

    primitive_types done

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -9,12 +9,14 @@
 // - If Mary buys more than 40 apples, each apple only costs 1 rustbuck!
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
-
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    if quantity <= 40 {
+        quantity * 2
+    } else {
+        quantity * 1
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -30,11 +30,27 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: vec![(str, Command)]) -> vec![str] {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: vec![str] = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            let mut string: str = string;
+            let command: Command = command;
+            match command {
+                Command::Append(usize) => {
+                    for _ in 0..usize {
+                        let mut string: &str = concat!(string, "bar");
+                    }
+                    output.push(string);
+                }
+                Command::Trim => {
+                    output.push(string.trim())
+                }
+                Command::Uppercase => {
+                    output.push(string.to_uppercase())
+                }
+            }
         }
         output
     }
@@ -43,7 +59,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use my_module;
     use super::Command;
 
     #[test]
